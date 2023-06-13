445eb4887d07a1d7535c39ef839b84d96d4a43cb
shaheen <84270559+shaheen-senpai@users.noreply.github.com>
2023-06-14 01:10:46 +0530

    Revert "Merge branch 'marahim20:master' into master"
    This reverts commit 5635f3189a5b9e15fc538c35a2608a9603d1c436, reversing
    changes made to 4062953d9b6714a7ca9124450d34a47fd7da8889.

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, useNavigate } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import Login from "./pages/login.jsx";
 import Register from "./pages/register.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Layout.jsx";
 
-const Main = () => {
-  const navigate = useNavigate();
-  const userID = localStorage.getItem("user_ID");
-
-  React.useEffect(() => {
-    if (!userID) {
-      navigate("/login");
-    }
-  }, [navigate, userID]);
-
-  return (
-    <React.StrictMode>
-      {userID ? <App /> : <Login />}
-    </React.StrictMode>
-  );
-};
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Main />
-  </BrowserRouter>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
