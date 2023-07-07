138bbd547bdca17b367a1825e3e2d4dbaf389860
marahim20 <minhajabdulrahim@gmail.com>
2023-07-08 01:08:39 +0530

    finalbuild

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import Login from "./pages/LoginPage";
-import Register from "./pages/RegisterPage";
+import Login from "./routes/LoginPage";
+import Register from "./routes/RegisterPage";
 import Loading from "./loading"
 
 function Main() {
